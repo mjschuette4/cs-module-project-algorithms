b563cd821e0f149385210b4e3e58b8894be2df92
unknown <mjschuette4@gmail.com>
2020-08-20 16:06:10 -0500

    First pass solution to eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,16 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    if n < 0:
+        return 0
+    # There is one way to eat no cookies
+    elif n == 0:
+        return 1
+    # To figure out cases above 0
+    else:
+        return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
-
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
